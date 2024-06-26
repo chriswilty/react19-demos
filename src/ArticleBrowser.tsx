b550ee9424c8754fbdde0c7f6f4d869994e711e9
Chris Wilton-Magras <chriswilty@zoho.com>
2024-06-26 14:22:41 +0100

    React 19 useTransition for data loading

--- src/ArticleBrowser.tsx
+++ src/ArticleBrowser.tsx
@@ -1,10 +1,9 @@
 import {
-	Suspense,
-	use,
 	useCallback,
 	useEffect,
 	useMemo,
 	useState,
+	useTransition,
 } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 
@@ -57,37 +56,32 @@ const ArticleView = ({
 };
 
 type ArticleListProps = {
-	itemsPromise: Promise<Array<Item>>;
+	items: Array<Item>;
 };
 
-const ArticleList = ({ itemsPromise }: ArticleListProps) => {
-	// This component will suspend until promise resolves
-	const items = use(itemsPromise);
-
-	return (
-		<main className="card">
-			<ul>
-				{items.map((item) => (
-					<li key={item.title}>
-						<ArticleView {...item} />
-					</li>
-				))}
-			</ul>
-		</main>
-	);
-};
+const ArticleList = ({ items }: ArticleListProps) => (
+	<main className="card">
+		<ul>
+			{items.map((item) => (
+				<li key={item.title}>
+					<ArticleView {...item} />
+				</li>
+			))}
+		</ul>
+	</main>
+);
 
 const ArticleBrowser = () => {
-	// React 19 with use(): We can now pass a promise of data to ArticleList, which will
-	// suspend until the promise is resolved.
-	// TODO Handle data insert after update: Unpack promise, add new item, set as new promise?
-	const [itemsPromise, setItemsPromise] = useState<Promise<Array<Item>>>(
-		Promise.resolve([])
-	);
+	// React 19 data loading with useTransition()
+	const [isLoading, startTransition] = useTransition();
+	const [items, setItems] = useState<Array<Item>>([]);
 
 	useEffect(() => {
 		const abortController = new AbortController();
-		setItemsPromise(service.fetchItems(abortController.signal));
+		startTransition(async () => {
+			const data = await service.fetchItems(abortController.signal);
+			setItems(data);
+		});
 		return () => abortController.abort();
 	}, []);
 
@@ -111,9 +105,7 @@ const ArticleBrowser = () => {
 				<h1>My Favourite Things</h1>
 			</header>
 			<ErrorBoundary fallback={errorMessage}>
-				<Suspense fallback={loader}>
-					<ArticleList itemsPromise={itemsPromise} />
-				</Suspense>
+				{isLoading ? loader : <ArticleList items={items} />}
 			</ErrorBoundary>
 		</div>
 	);
